34cfea1453deea00a15e1e1b5f003826f8b5a961
NeocortexF <eugeneshamkin@mail.ru>
2017-03-21 16:22:33 +0500

    uplifting of serv and test of jstl/config

---
+++ src/main/java/software/neocortex/config/application/WebConfig.java
@@ -0,0 +1,41 @@
+package software.neocortex.config.application;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"software.neocortex.config", "software.neocortex.controller.*", "software.neocortex.service.*", "software.neocortex.dao.*", "software.neocortex.*"})
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setContentType("text/html; charset=utf-8");
+        return viewResolver;
+    }
+
+    @Bean
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("WEB-INF/views/css");
+        registry.addResourceHandler("/img/**").addResourceLocations("WEB-INF/views/img");
+        registry.addResourceHandler("/js/**").addResourceLocations("WEB-INF/views/js");
+        registry.addResourceHandler("/font/**").addResourceLocations("WEB-INF/views/font");
+    }
+
+
+}
---
+++ web/WEB-INF/views/hello.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Test spring app</title>
+</head>
+<body>
+hello flying base 2!
+${bindingPoint}
+</body>
+</html>
