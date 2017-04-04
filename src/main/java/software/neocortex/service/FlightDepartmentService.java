c99092e1ca068e24195aaf8b48658c84c722e11c
NeocortexF <eugeneshamkin@mail.ru>
2017-04-04 11:35:32 +0500

    FD service methods implemention

--- src/main/java/software/neocortex/service/FlightDepartmentService.java
+++ src/main/java/software/neocortex/service/FlightDepartmentService.java
@@ -1,4 +1,24 @@
 package software.neocortex.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import software.neocortex.dao.FlightDepartmentDAO;
+import software.neocortex.entity.FlightDepartmentDataTable;
+
+import java.util.List;
+
+@Component
+@Transactional
 public class FlightDepartmentService {
+    @Autowired
+    FlightDepartmentDAO flightDepartmentDAO;
+
+    public FlightDepartmentDataTable getFlightDepartmentDataById(int id) {
+        return flightDepartmentDAO.getFlightDepartmentDataById(id);
+    }
+
+    public List<FlightDepartmentDataTable> getFlightDepartmentDataTable() {
+        return flightDepartmentDAO.getFlightDepartmentDataTable();
+    }
 }
